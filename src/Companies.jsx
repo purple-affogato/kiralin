d47696e9367041f8f092cbc90d1edbbbde70c331
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-03-16 18:51:10 -0400

    search engine worksssssssssssssssssssssss
    im crying tears of joy

--- src/Companies.jsx
+++ src/Companies.jsx
@@ -3,37 +3,53 @@ import { supabase } from "./supabaseClient";
 
 function Companies (){
     const [companies, setCompanies] = useState([]);
-    const [searchInput, setSearchInput] = useState("")
+    const [searchInput, setSearchInput] = useState("");
+    const [results, setResults] = useState([]);
 
     useEffect (() => {
         async function getCompanies(){
-            const {data: companies} = await supabase.from('companies').select('companyname');
+            const {data: companies} = await supabase.from('companies').select('id, companyname, industry');
             if (companies.length > 1){
                 setCompanies(companies);
-                console.log(companies);
-            } 
+                setResults(companies);
+            }
         }
         getCompanies();
     }, []);
 
+
     const handleChange = (e) => {
         e.preventDefault();
         setSearchInput(e.target.value);
-      };
+        if (e.target.value.length == 0){
+            setResults(companies);
+        }
+        else{
+            search(e.target.value);
+        }
+        
+    }
 
+    async function search(str) {
+        const {data, error} = await supabase.from('companies').select('id, companyname, industry').ilike('companyname','%'+str+'%');
+        console.log(data);
+        if (data.length > 0){
+            setResults(data);
+        }
+    }
 
     return(
         <div className="SiteBody">
-            <form action="" method="GET">
-                <input 
-                type="text" 
-                placeholder="Search company names here" 
-                onChange={handleChange}
-                value={searchInput}></input>
-            </form>
+            <input 
+            type="text" 
+            id="search"
+            name="search"
+            placeholder="Search company names here"
+            onChange={handleChange}
+            value={searchInput}/>
             
             <ul>
-                {companies.map(function(company) {
+                {results.map(function(company) {
                     return(
                         <li key={company.companyname}>{company.companyname}</li>
                     ); 
