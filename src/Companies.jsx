fc18caf8eb167218dc38c91216de5a66e9382afe
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-03-16 00:29:11 -0400

    i can now generate a list of companies

--- src/Companies.jsx
+++ src/Companies.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient";
 
 function Companies (){
+    const [companies, setCompanies] = useState([]);
+    const [searchInput, setSearchInput] = useState("")
+
+    useEffect (() => {
+        async function getCompanies(){
+            const {data: companies} = await supabase.from('companies').select('companyname');
+            if (companies.length > 1){
+                setCompanies(companies);
+                console.log(companies);
+            } 
+        }
+        getCompanies();
+    }, []);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+      };
+
+
     return(
         <div className="SiteBody">
-            <p>Add search bar here</p>
+            <form action="" method="GET">
+                <input 
+                type="text" 
+                placeholder="Search company names here" 
+                onChange={handleChange}
+                value={searchInput}></input>
+            </form>
+            
+            <ul>
+                {companies.map(function(company) {
+                    return(
+                        <li key={company.companyname}>{company.companyname}</li>
+                    ); 
+                })}
+            </ul>
+
         </div>
         
     );
