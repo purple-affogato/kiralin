edbaad849c3b4806a9f287d3bb38538271e5cb1c
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-03-20 01:29:06 -0400

    finally figured out voting thing for company page
    time to replicate this twice :skull:

--- src/CompanyPage.jsx
+++ src/CompanyPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "./supabaseClient";
-import Popup from 'reactjs-popup';
+import WorkplaceCulture from "./WorkplaceCulture";
 
 export default function CompanyPage() {
     let {companyID} = useParams();
@@ -32,71 +32,16 @@ export default function CompanyPage() {
             <h1>{company.companyname}</h1>
             <p>Industry: {company.industry}</p>
             {/* <p>Company ID: {companyID}</p> */}
-            <WorkplaceCulture y={company.wc1_y} n={company.wc1_n} pos={company.wc2_pos} neg={company.wc2_neg} />
+            <WorkplaceCulture y={company.wc1_y} n={company.wc1_n} pos={company.wc2_pos} neg={company.wc2_neg} id={companyID} />
             <h2>Employee Resource Groups (ERGs)</h2>
             <h2>Feedback Mechanisms</h2>
+
             <h2>Diversity Policies</h2>
+            <p>{company.dp1 ? "There diversity policies being implemented at this company." : "There are no diversity policies being implemented at this company."}</p>
+            <p>{company.dp2}</p>
             <h2>Representation in Leadership</h2>
+            <p>{company.rep_w} out of {company.rep_t} of this company's executive leadership are women or nonbinary people.</p>
         </div>
         
-    );
-}
-
-function WorkplaceCulture({y, n, pos, neg}){
-    let percent1 = Math.round(y/(y+n+0.0) * 1000.0) / 10.0;
-    let percent2 = Math.round(pos/(pos+neg+0.0) * 1000.0) / 10.0;
-    
-    const [option1, setOption1] = useState("");
-    const [option2, setOption2] = useState("");
-
-    function handleQ1Change(e) {
-        setOption1(e.target.value);
-    }
-
-    function handleQ2Change(e) {
-        setOption2(e.target.value);
-    }
-
-    function handleSubmit(){
-        if (option1 != "" && option2 != ""){
-            //update database & use cookies
-            close();
-        }
-        else{
-            alert("Please complete every poll to submit!");
-        }
-        
-    }
-
-    return(
-        <>
-            <h2>Workplace Culture</h2>
-            <p> {y+n == 0 ? 
-                "There is no data on how welcome female and nonbinary employees feel at this workplace yet." 
-                : percent1+"% of people on Kiralin felt welcome as a women or nonbinary person at this company."}
-            </p>
-
-            <p> {pos+neg == 0 ? 
-                "There is no data on how many female and nonbinary employees believe this company's workplace culture is positive or negative yet." 
-                : percent2+"% of people on Kiralin felt this company had a positive workplace culture."}
-            </p>
-
-            <p>If you've worked at or are currently working at this company, click the button to quickly share your experience!</p>
-            <Popup trigger={
-                <button className="popup-button">Vote</button>
-            } modal nested >
-                <>
-                    <h2>Vote here</h2>
-                    <p>Do you, as a women or nonbinary person, feel welcome at this company's workplace?</p>
-                    <label><input type="radio" name="q1" value={"y"} onChange={handleQ1Change}  />Yes</label>
-                    <label><input type="radio" name="q1" value={"n"} onChange={handleQ1Change}  />No</label>
-                    <hr/>
-                    <p>Is this company's workplace culture positive or negative?</p>
-                    <label><input type="radio" name="q2" value={"y"} onChange={handleQ2Change}  />Positive</label>
-                    <label><input type="radio" name="q2" value={"n"} onChange={handleQ2Change}  />Negative</label>
-                    <button className="popup-button" onClick={handleSubmit}>Submit</button>
-                </>
-            </Popup>
-        </>
     );
 }
---
+++ src/WorkplaceCulture.jsx
@@ -0,0 +1,89 @@
+import React, {useState} from "react";
+import Popup from 'reactjs-popup';
+import { supabase } from "./supabaseClient";
+
+export default function WorkplaceCulture({y, n, pos, neg, id}){
+    let percent1 = Math.round(y/(y+n+0.0) * 1000.0) / 10.0;
+    let percent2 = Math.round(pos/(pos+neg+0.0) * 1000.0) / 10.0;
+    
+    const [option1, setOption1] = useState("");
+    const [option2, setOption2] = useState("");
+    const [open, setOpen] = useState(false);
+
+    function handleQ1Change(e) {
+        setOption1(e.target.value);
+    }
+
+    function handleQ2Change(e) {
+        setOption2(e.target.value);
+    }
+
+    function handleSubmit(){
+        if (option1 != "" && option2 != ""){
+            //update database & use cookies
+            setOpen(false);
+            updateQ1Data();
+            updateQ2Data();
+        }
+        else{
+            alert("Please complete every poll to submit!");
+        }   
+    }
+
+    async function updateQ1Data(){
+        if (option1 == "y") {
+            const {data} = await supabase.from("companies").update({ wc1_y: y+1}).eq('id', id);
+            console.log(data);
+        }
+        else {
+            const {error} = await supabase.from('companies').update({ wc1_n: n+1}).eq('id', id);
+        }
+    }
+
+    async function updateQ2Data() {
+        if (option2 == "pos") {
+            const {error} = await supabase.from('companies').update({ wc2_pos: pos+1}).eq('id', id);
+        } else{
+            const {error} = await supabase.from('companies').update({ wc2_neg: neg+1}).eq('id', id);
+            // console.log(error);
+        }
+    }
+
+    const closePopup = () => {
+        setOpen(false);
+        setOption1("");
+        setOption2("");
+    }
+
+    return(
+        <>
+            <h2>Workplace Culture</h2>
+            <p> {y+n == 0 ? 
+                "There is no data on how welcome female and nonbinary employees feel at this workplace yet." 
+                : percent1+"% of people on Kiralin felt welcome as a women or nonbinary person at this company."}
+            </p>
+
+            <p> {pos+neg == 0 ? 
+                "There is no data on how many female and nonbinary employees believe this company's workplace culture is positive or negative yet." 
+                : percent2+"% of people on Kiralin felt this company had a positive workplace culture."}
+            </p>
+
+            <p>If you've worked at or are currently working at this company, click the button to quickly share your experience!</p>
+            <button type="button" className="popup-button" onClick={() => {setOpen(!open)}} >Vote</button>
+            <Popup open={open} modal nested closeOnDocumentClick onClose={closePopup}>
+                <>
+                    <h2>Vote here</h2>
+                    <p>Do you, as a women or nonbinary person, feel welcome at this company's workplace?</p>
+                    <label><input type="radio" name="q1" value={"y"} onChange={handleQ1Change}  />Yes</label>
+                    <label><input type="radio" name="q1" value={"n"} onChange={handleQ1Change}  />No</label>
+                    <br/>
+                    <p>Is this company's workplace culture positive or negative?</p>
+                    <label><input type="radio" name="q2" value={"pos"} onChange={handleQ2Change}  />Positive</label>
+                    <label><input type="radio" name="q2" value={"neg"} onChange={handleQ2Change}  />Negative</label>
+                    <hr/>
+                    <button className="popup-button" onClick={handleSubmit}>Submit</button>
+                </>
+            </Popup>
+        </>
+    );
+}
