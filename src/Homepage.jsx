93e883713a65b9484c9693bfd159b8eed2b5108a
purple-affogato <ivy.yiting.zhuang@gmail.com>
2024-03-21 00:08:35 -0400

    adjusted website description

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -5,17 +5,16 @@ function Homepage(){
         <div className="SiteBody">
             <h1>What is Kiralin</h1>
             <p>Kiralin is a site that allows users to assess how open and inclusive different companies are to women and nonbinary people. 
-                This is a submission for the Women in STEM Hackathon hosted by The STEAM Boat. The goal of this site is to make it easier
-                for women and nonbinary people working in STEM to navigate industries where they're a minority. You can get started by
-                clicking the "Companies" tab in the header and searching any company of your choosing!</p>
+                This is a submission for the Women in STEM Hackathon hosted by The STEAM Boat and Code Her World. The goal of this 
+                site is to make it easier for women and nonbinary people working in STEM to navigate industries where they're a minority. 
+                You can get started by clicking the "Companies" tab in the header and searching for any company of your choosing!</p>
             <h1>How does Kiralin work</h1>
             <p>Using a combination of our own research and user polls, Kiralin offers users a wide range of data to help them 
                 figure out which companies would be them as a women or nonbinary person. The research team (which is actually just one 
                 person right now) provides a score from 1 to 5 and a short explanation on each company's diversity policies and 
-                representation in leadership. Users can share their experiences by answering polls and leaving comments about 
+                representation in leadership. Users can share their experiences by answering polls about 
                 their experiences working at certain companies. Kiralin's polls are focused on workplace culture, employee resource 
-                groups, recruitment practices, and feedback mechanisms. All user data is kept anonymous and the site doesn't require an
-                account to use.</p>
+                groups, recruitment practices, and feedback mechanisms. All user data is kept anonymous.</p>
         </div>
     );
 }
